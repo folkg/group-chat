5a4f51679b4c730db40c3f8c2852c5002f822437
Graeme Folk <graemefolk@gmail.com>
2023-06-06 14:57:22 -0600

    initial socket.io implementation

--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,35 @@
-import express from "express";
+import express, { Express, Request, Response } from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import cors from "cors";
 
-const server = express();
-const hostname = "127.0.0.1";
-const port: number = 4000;
+const app: Express = express();
+app.use(express.json());
+app.use(cors());
 
-server.use(express.json());
-server.use(cors());
-
-server.get("/", (req, res) => {
+// TODO: Could remove express and just use socket.io if we aren't using express for anything else
+app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server!");
 });
 
-server.listen(port, hostname, () => {
+const httpServer = createServer(app);
+
+const io: Server = new Server(httpServer);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("join_room", (roomId) => {
+    console.log("user joined room #" + roomId);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+const hostname = "127.0.0.1";
+const port: number = 4000;
+httpServer.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
