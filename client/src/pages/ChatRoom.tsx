65c572f9aa8c6edbc6d452ee9980f3aeb8e9dcc3
Graeme Folk <graemefolk@gmail.com>
2023-06-07 15:37:18 -0600

    add localVideo to the chat

--- client/src/pages/ChatRoom.tsx
+++ client/src/pages/ChatRoom.tsx
@@ -1,7 +1,56 @@
+import { useContext, useEffect, useRef } from "react";
 import "./ChatRoom.css";
 import { useParams } from "react-router-dom";
+import { PeerContext } from "../contexts/peerConnection.context";
 
 export default function ChatRoom() {
   const { roomId } = useParams();
-  return <div>ChatRoom {roomId}</div>;
+  const peerConnection = useContext(PeerContext);
+  const localVideoRef = useRef<HTMLVideoElement>(null);
+  const remoteVideoRef = useRef<HTMLVideoElement>(null);
+
+  let localStream: MediaStream;
+  let remoteStream: MediaStream;
+
+  useEffect(() => {
+    initializeWebRTC();
+
+    async function initializeWebRTC() {
+      // Set up the local media stream
+      try {
+        localStream = await openMediaDevices({
+          video: true,
+          audio: true,
+        });
+        if (localVideoRef.current) {
+          localVideoRef.current.srcObject = localStream;
+        }
+      } catch (error) {
+        console.error("Error accessing media devices.", error);
+      }
+      localStream.getTracks().forEach((track: MediaStreamTrack) => {
+        peerConnection.addTrack(track, localStream);
+      });
+
+      // TODO: Handle multiple remotes
+      // Set up the remote media stream
+      remoteStream = new MediaStream();
+      peerConnection.addEventListener("track", (event: RTCTrackEvent) => {
+        event.streams[0].getTracks().forEach((track: MediaStreamTrack) => {
+          remoteStream.addTrack(track);
+        });
+      });
+    }
+  }, []);
+
+  return (
+    <>
+      <h1>ChatRoom {roomId}</h1>
+      <video ref={localVideoRef} autoPlay muted></video>
+    </>
+  );
+}
+
+async function openMediaDevices(constraints: MediaStreamConstraints) {
+  return await navigator.mediaDevices.getUserMedia(constraints);
 }
