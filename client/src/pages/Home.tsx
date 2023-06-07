97321f1f1889ea50d171ecb09bee9a2eb413b6db
Graeme Folk <graemefolk@gmail.com>
2023-06-06 20:47:09 -0600

    improve layout for clarity

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Button, TextField } from "@mui/material";
+import { Button, Card, TextField } from "@mui/material";
 import "./Home.css";
 import { useState } from "react";
 
@@ -18,33 +18,45 @@ export default function Home() {
   const isRoomIdValid = roomId.trim().length > 0;
 
   return (
-    <div>
+    <>
       <h1>Welcome to Group Chat</h1>
-      <div>
-        <div className="field-button-group">
-          <TextField
-            id="name"
-            label="Your Name"
-            value={name}
-            onChange={handleNameChange}
-          />
-          <Button variant="contained" disabled={!isNameValid || isRoomIdValid}>
-            Create New Room
-          </Button>
-        </div>
-        <p className="or-text">OR</p>
-        <div className="field-button-group">
-          <TextField
-            id="roomid"
-            label="Room ID"
-            value={roomId}
-            onChange={handleRoomIdChange}
-          />
-          <Button variant="contained" disabled={!isNameValid || !isRoomIdValid}>
-            Join Existing Room
-          </Button>
-        </div>
+      <div className="home-cards">
+        <Card className="home-card">
+          <div className="room-options">
+            <TextField
+              id="name"
+              label="Your Name"
+              value={name}
+              onChange={handleNameChange}
+            />
+          </div>
+        </Card>
+        <Card className="home-card">
+          <div className="room-options">
+            <Button
+              variant="contained"
+              disabled={!isNameValid || isRoomIdValid}
+            >
+              Create New Room
+            </Button>
+          </div>
+          <p className="or-text">OR</p>
+          <div className="room-options">
+            <TextField
+              id="roomid"
+              label="Room ID"
+              value={roomId}
+              onChange={handleRoomIdChange}
+            />
+            <Button
+              variant="contained"
+              disabled={!isNameValid || !isRoomIdValid}
+            >
+              Join Existing Room
+            </Button>
+          </div>
+        </Card>
       </div>
-    </div>
+    </>
   );
 }
